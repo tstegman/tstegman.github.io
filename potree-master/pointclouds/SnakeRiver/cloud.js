61da4ad89e983c916d3da9b8daf9c89ae32ca85a
tstegman <toby.stegman@gmail.com>
2017-04-02 16:44:23 -0700

    Snake Update

---
+++ potree-master/pointclouds/SnakeRiver/cloud.js
@@ -0,0 +1,28 @@
+{
+	"version": "1.6",
+	"octreeDir": "data",
+	"boundingBox": {
+		"lx": -289.163678,
+		"ly": -869.979396,
+		"lz": 1828.221063,
+		"ux": 1036.740315,
+		"uy": 455.924597,
+		"uz": 3154.125056
+	},
+	"tightBoundingBox": {
+		"lx": -224.563678,
+		"ly": -787.914896,
+		"lz": 1838.691063,
+		"ux": 427.172818,
+		"uy": 83.354099,
+		"uz": 1945.470062
+	},
+	"pointAttributes": [
+		"POSITION_CARTESIAN",
+		"COLOR_PACKED",
+		"CLASSIFICATION"
+	],
+	"spacing": 10.358625,
+	"scale": 0.010000,
+	"hierarchyStepSize": 5
+}
