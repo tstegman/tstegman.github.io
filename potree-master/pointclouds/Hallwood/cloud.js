b70c39fe51aaa36ba65691d31bf4acbd61fcd5a1
tstegman <toby.stegman@gmail.com>
2017-04-02 14:23:26 -0700

    first website

---
+++ potree-master/pointclouds/Hallwood/cloud.js
@@ -0,0 +1,28 @@
+{
+	"version": "1.6",
+	"octreeDir": "data",
+	"boundingBox": {
+		"lx": -3295.427251,
+		"ly": -2346.097648,
+		"lz": -77.448888,
+		"ux": 2546.190958,
+		"uy": 3495.520561,
+		"uz": 5764.169321
+	},
+	"tightBoundingBox": {
+		"lx": -2816.990828,
+		"ly": -2071.327648,
+		"lz": -71.848888,
+		"ux": 1972.651609,
+		"uy": 905.672025,
+		"uz": 86.696396
+	},
+	"pointAttributes": [
+		"POSITION_CARTESIAN",
+		"COLOR_PACKED",
+		"CLASSIFICATION"
+	],
+	"spacing": 45.637642,
+	"scale": 0.010000,
+	"hierarchyStepSize": 5
+}
